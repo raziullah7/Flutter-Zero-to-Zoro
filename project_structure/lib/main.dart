27f9ad905c4508381fa609b8bad78004bd675b42
Razi Ullah <raziullah913@gmail.com>
2024-02-10 01:17:30 +0500

    Project # 1: => learned Scaffold, Center and Text widgets => learned code formatting conventions in flutter

--- project_structure/lib/main.dart
+++ project_structure/lib/main.dart
@@ -6,25 +6,37 @@ import 'package:flutter/material.dart';
 
 // function with named arguments
 // add [] around positional argument(s) to make them optional
-void sum1(num1, num2) {
+void sum1(int num1, int num2) {
   num1 + num2;
 }
 
 // function with named arguments (named arguments are optional)
 // add the 'required' before the named argument(s) to make them compulsory
-void sum2({num1, num2}) {
+void sum2({int num1 = 0, int num2 = 0}) {
   num1 + num2;
 }
 
 void main() {
+  // the main start of the file
+  runApp(
+    const MaterialApp(
+      title: 'Flutter App',
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    ),
+  );
+
   // positional nature of arguments can cause problems
   // we overwrite the positional nature by adding {} in the definition
   // sum1(12, 8);
 
   // in this invocation, we have to add the identifier labels of arguments
-  // both of the below work becasue named arguments are optional
+  // all of the below work becasue named arguments are optional
   // sum2(num1: 12, num2: 8);
+  // sum2(num1: 12);
+  // sum2(num2: 8);
   // sum2();
-
-  runApp(const MaterialApp(home: Text('Hello World!')));
 }
